4fb031ec0db2418fbdafdfd9ac3771b04b441210
Elias Dobrin <elias@dobrin.net>
2025-03-03 16:12:20 -0800

    added test images

--- bld.sh
+++ bld.sh
@@ -1,4 +1,4 @@
 mkdir -p bin
-cc src/ngl.c src/img.c -o bin/ngl -std=c2x
-cc src/png2.c src/img.c -o bin/png2 -lm -std=c2x
-cc src/2png.c src/img.c -o bin/2png -lm -std=c2x
+gcc src/ngl.c src/img.c -o bin/ngl -std=c2x
+gcc src/png2.c src/img.c -o bin/png2 -lm -std=c2x
+gcc src/2png.c src/img.c -o bin/2png -lm -std=c2x
