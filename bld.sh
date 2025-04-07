3339967380e9feb3872537a84f9a8a735a6aa4d2
Elias Dobrin <elias@dobrin.net>
2025-04-07 15:14:02 -0700

    scrap and redo project

--- bld.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p bin
-gcc src/ngl.c src/img.c -o bin/ngl -std=c2x
-gcc src/png2.c src/img.c -o bin/png2 -lm -std=c2x
-gcc src/2png.c src/img.c -o bin/2png -lm -std=c2x
--- src/img.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int ench(int w, int h, FILE* file);
-int dech(int* w, int* h, FILE* file);
-int encb(int w, int h, float img[w][h][3], FILE* file);
-int decb(int w, int h, float img[w][h][3], FILE* file);
